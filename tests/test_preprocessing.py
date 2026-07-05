2cbd50cd9df286f5762b46641a635a542b3bdf57
Blen Assefa <blenasefa01@gmail.com>
2026-07-05 06:10:29 -0700

    Delete tests directory

--- tests/test_preprocessing.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from src.preprocessing import check_stationarity
-
-def test_stationarity():
-    # Create a random walk series
-    np.random.seed(42)
-    series = pd.Series(np.cumsum(np.random.randn(100)))
-    result = check_stationarity(series)
-    assert result['stationary'] == False
