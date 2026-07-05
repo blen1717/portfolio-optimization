51f3c315038d1cd5739b71b6ba48f1fc8cc3776d
Blen Assefa <blenasefa01@gmail.com>
2026-07-05 06:10:06 -0700

    Delete src directory

--- src/portfolio.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from pypfopt import EfficientFrontier
-
-def optimize_portfolio(expected_returns, cov_matrix, risk_free_rate=0.02):
-    ef = EfficientFrontier(expected_returns, cov_matrix)
-    weights = ef.max_sharpe(risk_free_rate=risk_free_rate)
-    return ef.clean_weights()
